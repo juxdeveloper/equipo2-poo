3a1934e65f51c7cffcac3d0788c389697ad87b5a
thxp2 <hannielcardoso125@gmail.com>
2026-08-25 16:21:13 -0600

    añadir resta.java

---
+++ Resta.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Resta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa el primer entero a operar => ");
+        int x = scanner.nextInt();
+        System.out.println("Ingresa el primer entero a operar => ");
+        int y = scanner.nextInt();
+        System.out.println("Ingresa la operacion a realizar => (1) num 1 - num 2 / (2) num 2 - num 1");
+        int opc = scanner.nextInt();
+        if(opc == 1){
+            int resultado = x - y;
+            System.out.println("El resultado es => " + resultado);
+        }else{
+            int resultado = y - x;
+            System.out.println("El resultado es => "+resultado);
+        }
+        scanner.close();
+    }
+}
