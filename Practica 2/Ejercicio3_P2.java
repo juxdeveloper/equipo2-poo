ea565e524384b2c22da14edd95b890de01474999
thxp2 <hannielcardoso125@gmail.com>
2026-09-01 22:59:02 -0600

    agregar ejercicio3_p2

---
+++ Practica 2/Ejercicio3_P2.java
@@ -0,0 +1,10 @@
+
+public class Ejercicio3_P2 {
+
+    public static void main(String args[]) {
+        System.out.println("n \t 10*n \t 100*n \t 1000*n");
+        for(int i = 0; i < 5 ; i++){
+            System.out.println( (i+1) + " \t  " + 10*(i+1) + " \t  " + 100*(i+1) + " \t  " + 1000*(i+1));
+        }
+    }
+}
