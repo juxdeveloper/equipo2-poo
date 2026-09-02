7dd7d242ccc8d7b3fc344a78057ff2d331ffa413
Angel Joseph <juxdeveloper@gmail.com>
2026-09-01 21:30:39 -0600

    Fix: Ejercicio 1 más simple

--- Practica 2/Ejercicio1_P2.java
+++ Practica 2/Ejercicio1_P2.java
@@ -8,26 +8,20 @@ public class AnalisisResultados {
 
         int aprobados = 0;
         int reprobados = 0;
-        int estudiante = 1;
-        int resultado = 0;
 
-        while (estudiante <= 10) {
+        for (int estudiante = 1; estudiante <= 10; estudiante++) {
 
-            System.out.print("Estudiante " + estudiante + " (1=aprobó, 2=reprobó): ");
-            resultado = entrada.nextInt();
+            int resultado;
 
-            while (resultado != 1 && resultado != 2) {
-                System.out.println("Entrada inválida. Escribe 1 o 2.");
-                System.out.print("Intenta de nuevo: ");
+            do {
+                System.out.print("Estudiante " + estudiante + " (1=aprobó, 2=reprobó): ");
                 resultado = entrada.nextInt();
-            }
+            } while (resultado != 1 && resultado != 2);
 
             if (resultado == 1)
                 aprobados++;
             else
                 reprobados++;
-
-            estudiante++;
         }
 
         System.out.println("\n--- Resumen ---");
